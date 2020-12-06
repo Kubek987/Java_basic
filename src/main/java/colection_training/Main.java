89696908216bdd2ba2db78390c5f899804d581ca
kubek987 <bonkowski183@gmail.com>
2020-12-06 10:49:54 +0100

    set training done

--- src/main/java/colection_training/Main.java
+++ src/main/java/colection_training/Main.java
@@ -2,6 +2,9 @@ package colection_training;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("ListTraining");
         ListTraining.trainList();
+        System.out.println("setTraining");
+        SetTraining.trainSet();
     }
 }
